095d6576498a4017a3235cf7a4f5454fd97c0adb
Marcus Stollsteimer <sto.mar@web.de>
2017-01-01 10:58:03 +0100

    Update copyright information

--- test/test_string.rb
+++ test/test_string.rb
@@ -1,6 +1,6 @@
 # test_string.rb: Unit tests for the numren script.
 #
-# Copyright (C) 2011-2016 Marcus Stollsteimer
+# Copyright (C) 2011-2017 Marcus Stollsteimer
 
 require 'minitest/autorun'
 load 'numren'  unless defined?(Numren)
