6e4790bc4c26e903b28035392837f46ed47e2c32
Marcus Stollsteimer <sto.mar@web.de>
2023-12-28 13:59:20 +0100

    Rename is_i? to integer?

--- test/test_string.rb
+++ test/test_string.rb
@@ -8,26 +8,26 @@ describe String do
 
   describe "when asked if string represents an integer" do
     it "should recognize valid integers" do
-      _("3".is_i?).must_equal true
-      _("01".is_i?).must_equal true
-      _("42".is_i?).must_equal true
-      _("123".is_i?).must_equal true
-      _("0".is_i?).must_equal true
+      _("3".integer?).must_equal true
+      _("01".integer?).must_equal true
+      _("42".integer?).must_equal true
+      _("123".integer?).must_equal true
+      _("0".integer?).must_equal true
     end
 
     it "should recognize non-integers" do
-      _("A".is_i?).must_equal false
-      _("0A".is_i?).must_equal false
-      _("A0".is_i?).must_equal false
-      _("".is_i?).must_equal false
+      _("A".integer?).must_equal false
+      _("0A".integer?).must_equal false
+      _("A0".integer?).must_equal false
+      _("".integer?).must_equal false
     end
 
     it "should recognize signed integers" do
-      _("+5".is_i?).must_equal true
-      _("-2".is_i?).must_equal true
-      _("+02".is_i?).must_equal true
-      _("1+2".is_i?).must_equal false
-      _("3-2".is_i?).must_equal false
+      _("+5".integer?).must_equal true
+      _("-2".integer?).must_equal true
+      _("+02".integer?).must_equal true
+      _("1+2".integer?).must_equal false
+      _("3-2".integer?).must_equal false
     end
   end
 end
